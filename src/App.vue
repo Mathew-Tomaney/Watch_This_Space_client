bdd2f306cdbf83d228845df9ad1c8360c76042f6
CatAnderson <thecatanderson100@gmail.com>
2020-11-01 15:46:11 +0000

    final branch commit

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import PlanetService from './services/PlanetService.js'
+import PlanetService from "./services/PlanetService.js";
 
 import Header from "./components/Header.vue";
 // import SolarSystem from "./components/SolarSystem.vue";

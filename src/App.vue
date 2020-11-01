76bdd857d277c1e575ee21f01d6b9bd96a1bef31
CatAnderson <thecatanderson100@gmail.com>
2020-11-01 14:32:15 +0000

    header added with nav bar, little css style to see if it works

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<template>
+<template lang="html">
+<main>
+  <header-component></header-component>
   <h1>Watch This Space Frontend</h1>
+</main>
 </template>
 
 <script>
-export default {
 
+import Header from "./components/Header.vue";
+
+export default {
+  name: 'app',
+
+  components: {
+    'header-component' : Header
+  }
 }
 </script>
 
-<style>
+<style lang="css">
 
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,34 @@
-<template>
-  
+<template lang="html">
+  <header>
+    <h1>watch this space</h1>
+    <svg id='nav-bar' viewBox="0 0 100 80" width="40" height="40">
+    <rect width="100" height="15"></rect>
+    <rect y="30" width="100" height="15"></rect>
+    <rect y="60" width="100" height="15"></rect>
+    </svg>  
+  </header>
 </template>
 
 <script>
 export default {
-
+  name:'header-component'
 }
 </script>
 
-<style>
+<style lang="css" scoped>
+
+header {
+  display: flex;
+  flex-flow: wrap row;
+  justify-content:flex-start;
+  background-color: #556272;
+
+  padding: 10px;
+}
+
+#nav-bar{
+  justify-self: flex-end;
+  padding: 20px;
+}
 
 </style>

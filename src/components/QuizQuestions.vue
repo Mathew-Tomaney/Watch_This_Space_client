40d602986c5996b7dd4feefffb129d385f1cddf3
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-03 11:14:35 +0000

    question for each selected planet is now rendering to browser

--- src/components/QuizQuestions.vue
+++ src/components/QuizQuestions.vue
@@ -1,17 +1,27 @@
 <template lang="html">
     <div>
-        {{selectedPlanet.name}}
+        <h3>{{selectedPlanet.quiz.questions[0].question}}</h3>
+        ul
     </div>
 </template>
 
 <script>
+
 export default {
     name: 'quiz-questions',
     props: ['planets', 'selectedPlanet']
 
+
+
+
 }
+
 </script>
 
 <style lang="css" scoped>
+
+h3 {
+    color: white;
+}
 
 </style>
